1b61aca3dd208004d31ae453cf8675c4642ad5be
cruxbreaker <geekankit318@gmail.com>
2020-12-02 02:06:51 +0530

    Add custom hook useContent to fetch content

--- src/pages/browse.tsx
+++ src/pages/browse.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import { useContent } from '../hooks';
 
 const Browse = () => {
+  const { series } = useContent('series');
+  const { films } = useContent('films');
+  console.log(series, films);
   return (
     <>
       <p>Browse</p>
